d8cdc2db0fd78bc2be5d98e82b9c9dcb52febd88
maikelsouza <maikel.souza@gmail.com>
2021-09-07 12:41:25 -0300

    Primeiro commit do curso -  Spring Boot API REST: Construa uma API

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	public List<Topico> findByCurso_Nome(String nomeCurso);
+	
+	public List<Topico> findByCursoNome(String nomeCurso);
+
+}
